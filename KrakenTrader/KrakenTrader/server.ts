dd69bafb153e40c86c46722e5eccc6327ffca69d
Emil Wasilewski <me@limered.de>
2017-09-13 19:24:06 +0200

    initial setup

---
+++ KrakenTrader/KrakenTrader/server.ts
@@ -0,0 +1,10 @@
+﻿import * as http from 'http';
+var port = process.env.port || 1337;
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
+
+(async () => {
+    await "Hallo";
+});
