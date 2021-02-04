e2bef5a9f8c333db3332744d611742f517814bf1
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-02-04 22:40:26 +0100

    remove old stuff

--- KrakenTrader/KrakenTrader/js/starter.js
+++
@@ -1,6 +0,0 @@
-var socket = io('http://localhost:80');
-socket.on('test', data => {
-    var container = document.querySelector('#container');
-    container.appendChild(new Text(JSON.stringify(data)));
-});
-//# sourceMappingURL=starter.js.map
--- KrakenTrader/KrakenTrader/js/starter.ts
+++
@@ -1,6 +0,0 @@
-﻿var socket = io('http://localhost:80');
-
-socket.on('test', data => {
-    var container = document.querySelector('#container');
-    container.appendChild(new Text(JSON.stringify(data)));
-});
